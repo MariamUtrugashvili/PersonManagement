a35dcaf5cee9111369b7317e4983ba2f33f718b0
MariamUtrugashvili <116346106+MariamUtrugashvili@users.noreply.github.com>
2025-07-22 01:40:19 +0300

    add global exception handler

---
+++ src/PersonManagement.Api/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using PersonManagement.Application.Exceptions;
+using System.Net;
+
+namespace PersonManagement.Api.Middlewares
+{
+    public class GlobalExceptionHandlerMiddleware(
+        RequestDelegate next,
+        ILogger<GlobalExceptionHandlerMiddleware> logger)
+    {
+        private readonly RequestDelegate _next = next;
+        private readonly ILogger<GlobalExceptionHandlerMiddleware> _logger = logger;
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unhandled exception occurred");
+
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            // Default to 500
+            var statusCode = HttpStatusCode.InternalServerError;
+            var message = "An unexpected error occurred.";
+
+            switch (exception)
+            {
+                case PersonNotFoundException pnfEx:
+                    statusCode = HttpStatusCode.NotFound;
+                    message = pnfEx.Message;
+                    break;
+            }
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)statusCode;
+
+            return context.Response.WriteAsync(message);
+        }
+    }
+}
--- src/PersonManagement.Api/Program.cs
+++ src/PersonManagement.Api/Program.cs
@@ -1,12 +1,21 @@
+using PersonManagement.Api.Middlewares;
+using PersonManagement.Application;
+using PersonManagement.Application.Persons.Commands.CreatePerson;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddApplicationServices();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
