5601993c03fada209e4d96333dcdb3d6bbc69da3
MariamUtrugashvili <116346106+MariamUtrugashvili@users.noreply.github.com>
2025-07-22 01:41:34 +0300

    add fluent validations in application layer

---
+++ src/PersonManagement.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using PersonManagement.Application.Persons.Commands.CreatePerson;
+
+namespace PersonManagement.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssemblyContaining<CreatePersonCommandValidator>();
+
+            return services;
+        }
+    }
+}
