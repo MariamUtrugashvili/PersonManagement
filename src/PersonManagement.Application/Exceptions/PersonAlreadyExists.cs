01377e0e12d27d10722e9f96dceedee049b3c0fd
MariamUtrugashvili <116346106+MariamUtrugashvili@users.noreply.github.com>
2025-07-22 16:06:49 +0300

    add custom exceptions

---
+++ src/PersonManagement.Application/Exceptions/PersonAlreadyExists.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonManagement.Application.Exceptions
+{
+    public class PersonAlreadyExists(string personalNumber) :
+        Exception($"A person with this personal number {personalNumber} already exists")
+    {
+    }
+
+}
---
+++ src/PersonManagement.Application/Exceptions/RelationAlreadyExistsException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonManagement.Application.Exceptions
+{
+    public class RelationAlreadyExistsException() :
+        Exception("This relation already exists")
+    {
+    }
+}
