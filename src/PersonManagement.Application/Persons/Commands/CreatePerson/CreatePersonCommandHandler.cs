e2be11278af19d64f60d0566ad19423550da81cd
MariamUtrugashvili <116346106+MariamUtrugashvili@users.noreply.github.com>
2025-07-23 19:29:28 +0300

    remove cache invalidation from create person

--- src/PersonManagement.Application/Persons/Commands/CreatePerson/CreatePersonCommandHandler.cs
+++ src/PersonManagement.Application/Persons/Commands/CreatePerson/CreatePersonCommandHandler.cs
@@ -2,18 +2,14 @@ using MediatR;
 using PersonManagement.Application.Exceptions;
 using PersonManagement.Domain.Entities;
 using PersonManagement.Domain.Repositories;
-using PersonManagement.Application.Caching;
-
 namespace PersonManagement.Application.Persons.Commands.CreatePerson
 {
     public class CreatePersonCommandHandler : IRequestHandler<CreatePersonCommand, CreatePersonResponse>
     {
         private readonly IPersonRepository _personRepository;
-        private readonly ICacheService _cacheService;
-        public CreatePersonCommandHandler(IPersonRepository personRepository, ICacheService cacheService)
+        public CreatePersonCommandHandler(IPersonRepository personRepository)
         {
             _personRepository = personRepository;
-            _cacheService = cacheService;
         }
 
         public async Task<CreatePersonResponse> Handle(CreatePersonCommand request, CancellationToken cancellationToken)
@@ -38,9 +34,6 @@ namespace PersonManagement.Application.Persons.Commands.CreatePerson
             
             await _personRepository.AddAsync(person, cancellationToken);
             await _personRepository.SaveChangesAsync(cancellationToken);
-
-            var cacheKey = $"person:{person.Id}";
-            await _cacheService.RemoveAsync(cacheKey);
 
             return new CreatePersonResponse { Id = person.Id };
         }
