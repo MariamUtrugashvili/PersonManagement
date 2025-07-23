6537c2958e4a730ab0566747e718b7500b50df29
MariamUtrugashvili <116346106+MariamUtrugashvili@users.noreply.github.com>
2025-07-23 14:30:48 +0300

    add related person configuration migration

---
+++ src/PersonManagement.Persistence/Migrations/20250723092505_UpdateReletedPersonConfiguration.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonManagement.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateReletedPersonConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RelatedPersons_Persons_PersonId",
+                table: "RelatedPersons");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RelatedPersons_RelatedToPersonId",
+                table: "RelatedPersons",
+                column: "RelatedToPersonId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RelatedPersons_Persons_PersonId",
+                table: "RelatedPersons",
+                column: "PersonId",
+                principalTable: "Persons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RelatedPersons_Persons_RelatedToPersonId",
+                table: "RelatedPersons",
+                column: "RelatedToPersonId",
+                principalTable: "Persons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RelatedPersons_Persons_PersonId",
+                table: "RelatedPersons");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RelatedPersons_Persons_RelatedToPersonId",
+                table: "RelatedPersons");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RelatedPersons_RelatedToPersonId",
+                table: "RelatedPersons");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RelatedPersons_Persons_PersonId",
+                table: "RelatedPersons",
+                column: "PersonId",
+                principalTable: "Persons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
